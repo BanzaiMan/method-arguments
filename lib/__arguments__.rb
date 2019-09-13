5f05b8e07c0bb194792ccd84551ecd8e63e18770
Hiro Asari <asari.ruby@gmail.com>
2019-09-13 15:58:41 -0400

    Add documentation

--- lib/__arguments__.rb
+++ lib/__arguments__.rb
@@ -1,4 +1,8 @@
 module Arguments
+  # Returns an array indicating the arguments given tothe current method.
+  #
+  # @param bdg [Binding] binding in the current scope
+  # @return [Array] array of 2-element arrys of the form `[symbol, value]`
   def __arguments__(bdg)
     method_symbol = caller(1,1).first.match(/\`(?<name>[^']*)'/)[:name].to_sym
     self.method(method_symbol).parameters.map { |e| [e[1], bdg.local_variable_get(e[1])] }
